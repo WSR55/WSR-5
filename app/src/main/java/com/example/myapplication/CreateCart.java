9400a9379d155b81c412dc260612653324f73d4e
WSR_5 <annadenisova65@gmail.com>
2023-03-21 17:59:04 +0800

    2 сессия

--- app/src/main/java/com/example/myapplication/CreateCart.java
+++ app/src/main/java/com/example/myapplication/CreateCart.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateCart extends AppCompatActivity {
 //
@@ -10,5 +12,11 @@ public class CreateCart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_cart);
+    }
+    //21.03.2023 17:57 5 участник. Денисова Анна. Сессия 2
+    public void zap(View view) {
+        //объявляем intent, выделяем для него память, указываем на какой странице мы находимся и на какую переходим
+        Intent intent = new Intent(CreateCart.this, CreateCartZap.class);
+        startActivity(intent);
     }
 }
